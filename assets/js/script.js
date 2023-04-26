cd4dc5d7fb05ce50fbdb3e7112014bc1e739ec4a
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-04-25 21:18:15 -0300

    113º dia de CTD - Avançando no Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const dropList= document.querySelectorAll(".drop-list select"),
-getButton = document.querySelectorAll("form button");
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+const apiKey = 'd763d5db5a0d7591583cd251';
 
 for(let i = 0; i < dropList.length; i++) {
     for(currency_code in country_code){
@@ -15,13 +19,59 @@ for(let i = 0; i < dropList.length; i++) {
         // inserting options tag inside select tag
         dropList[i].insertAdjacentHTML("beforeend", optionTag)
     }
+    dropList[i].addEventListener("change", e =>{
+       loadFlag(e.target); // Calling loadFlag with passing target element as an argument
+    }); 
 }
 
-getButton.addEventListener("click", e =>{
+function loadFlag(element){
+    for(code in country_code){
+        if(code == element.value){ //if currency code of country list is equal to option value
+            let imgTag = element.parentElement.querySelector("img"); // Selecting img tag of particular drop list
+            // Passing country code of a selected currency code in a img url
+            imgTag.src = `https://flagsapi.com/${country_code[code]}/flat/64.png`
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e => {
     e.preventDefault(); //preventing form from submitting
     getExchangeRate();
 });
 
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value; // Temporary currency code of FROM drop list
+    fromCurrency.value = toCurrency.value; // Passing TO currency code to FROM currency code
+    toCurrency.value = tempCode; // Passing temprary currency code to TO currency code
+    loadFlag(fromCurrency); // calling loadFlag with passing select element (fromCurrency) ofFROM
+    loadFlag(toCurrency); // calling loadFlag with passing select element (toCurrency) of TO
+    getExchangeRate();
+})
+
+
 function getExchangeRate(){
-    const amount = document.querySelector("")
+    const amount = document.querySelector(".amount input"),
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
+    // fetching api response and returning it with parsing into js obj and in another then method receiving that obj 
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+        // if user is offline or other error occured while fecting data then catch function will run
+    }).catch(() =>{
+        exchangeRateTxt.innerText = "Something went wrong"       
+    })
 }
+
